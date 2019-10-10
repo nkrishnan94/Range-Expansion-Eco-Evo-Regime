a0d9cb354bdbe9733f958d227788db21ddb88820
nkrishnan94 <npk13@case.edu>
2019-10-10 12:14:13 -0400

    Add files via upload
    Improved annotation, clarity of notebook and script

--- diffusion_growth_funcs.py
+++ diffusion_growth_funcs.py
@@ -4,6 +4,16 @@ from scipy.integrate import odeint
 import scipy.integrate as integrate
 from scipy.interpolate import interp1d
 
+
+
+##
+def standing_wave(y0,x,D,rw):
+    u = y0[0]
+    z = y0[1]
+    dudx = z
+    dzdx =(-2*((rw*D)**.5)*dudx -u*rw*(1-u))/D
+    return [dudx,dzdx]
+
 ###'simplest' cases
 def simple_fisher(u,t,D, r,dt):
     ##classic 1D fisher equation with diffusion constant D, growth rate r, population density u 
@@ -31,65 +41,8 @@ def surf_prob_solve(b,rw,rm,D, du0 ):
     s =odeint(surf_prob,[0,du0],x,args = (rw,rm,D,bfunc))
     return s
 
-def standing_wave(y0,x,D,rw):
-    u = y0[0]
-    z = y0[1]
-    dudx = z
-    dzdx =(-2*((rw*D)**.5)*dudx -u*rw*(1-u))/D
-
-    return [dudx,dzdx]
 
 
-
-from scipy.interpolate import interp1d
-
-
-
-def surf_prob(y0,x,rw,rm,D,bfunc):
-    u = y0[0]
-    z = y0[1]
-    
-    
-    
-    dudx = z
-    dzdx =(-(2*((D*rw)**.5))*dudx+ u*(rm)*(1-bfunc(x))-u**2)/D
-
-    return [dudx,dzdx]
-
-
-
-def surf_prob_solve(b,rw,rm,D, du0 ):
-    bfunc = interp1d(range(len(b)),b, bounds_error=False, fill_value="extrapolate")
-    x = np.linspace(0,len(b)-1,len(b))
-    s =odeint(surf_prob,[0,du0],x,args = (rw,rm,D,bfunc))
-    return s 
-
-    
-    
-
-def mut_growth_wframe(y0, t,D,r,f,s,R):
-    b = y0[0:R]
-    bm= y0[R:]
-    b[b<0] = 0
-    bm[bm<0] = 0
-    dbmdt = bm*(r+f)*(1*s-(b+bm))+2*grad(bm,edge_order=2)*(D*(r))**.5+ D*grad(grad(bm,edge_order=2),edge_order=2)
-
-    dbdt= b*(r)*(1*s-(b+bm))+2*grad(b,edge_order=2)*(D*(r+0))**.5 
-    return np.concatenate([dbdt,dbmdt])
-
-
-def log_growth(z,t,D,r,f,s,R):
-    ###logrithmic growth of 2 populations with max growth rate r and r+f 
-    z[z<0] = 0
-    b = z[0:R]
-    bm = z[R:]
-
-    
-    dbdt =  D*grad(grad(b)) + b*r*(1*s-(b+bm))
-    dbmdt =  D*grad(grad(bm))+ bm*(r+f)*(1*s-(bm+b))
-
-
-    return np.concatenate([dbdt,dbmdt])
 
 
     ##functions for modified Croze et al. chemotaxis equations, with stochastic growth 
@@ -259,62 +212,8 @@ def sde_model_solve_1mut(R, R_div,c,c_0,c_1,delta,I,K_x, K_s,N, H,rw,a,mu,b0,s0,
     for i in t:
         btot = results[-1][0:R]+results[-1][R:2*R]+results[-1][2*R:3*R]
 
-        results.append(results[-1] +dt*model_sde_2mut(results[-1], 0,R,R_div,c,c_0,c_1,delta,I,K_x,K_s,N,H,rw,a,mu))
+        results.append(results[-1] +dt*model_sde_1mut(results[-1], 0,R,R_div,c,c_0,c_1,delta,I,K_x,K_s,N,H,rw,a,mu,K))
         results[-1][results[-1]< 0]= 0
         
     return results
 
-
-#####calculate fixation times and establishment times
-
-##solve log growth ode
-def ode_fix(b0,bm0,s,t,r,f,D, R,CC):
-    z0  = [b0,bm0]
-    results = odeint(log_growth_wframe,z0,t,args = (D,r,f,s,R))
-    fix_ind = np.where(((results[:,1]/results[:,0]) >10)*(results[:,0]*CC <1)*(results[:,1]*CC >1))[0]
-    return fix_ind, results
-
-###predicted fixation time for array of initial pops and substrate conc
-def calc_fix_times(b0,s0,K_s,t_max,t_step,r,f,CC):
-    G0 = s0/(s0+K_s)
-    t= np.linspace(0,t_max,t_max*t_step )
-    fix_times = []
-    for i in range(len(b0)):
-        fix_inds = ode_fix(b0[i],1/CC, G0[i],t,r,f,CC)[0]
-        if len(fix_inds) == 0:
-            time = 'inf'
-        else:
-            time = fix_inds[0] 
-                
-        fix_times.append(time)
-    return fix_times
-
-def diff_fix(bm,b,r,f,D):
-    dzdt = -(1/D)((r+f)(1*s - b)*bm + z*2*(D*r)**.5)
-    dbmdt = z
-    return np.concatenate(dzdt,dbmdt)
-
-
-###probability of fixation of mutant for a logistically growing population (kimura 1973)
-def fixation_prob(p,s,Z):
-    u = (1-np.exp(-4*Z*s*p))/(1-np.exp(-4*Z*s))
-    return u
-
-### calculate Z(N) - ideal stable population size (kimura 1973)
-def Z_N(s,r,N,K):
-    c = (s/r)
-    Q = (N/K)
-    numerator =np.array( N *(( Q**(-1-c)) * (1-Q)**c))
-    denom = c *np.array(integrate.quad(lambda q: (q**(-2-c)) * (1-q)**(c-1),Q,1))[0]
-    return (numerator/denom)
-
-##predicted establishment times for array of inital pop and substrate concentration 
-def calc_est_times(b0,s0,f,CC,mu,K_s):
-    r_s = (s0/(K_s+s0))
-    Zs = np.array([Z_N(f*r,1*r,b*CC,CC) for b,r in zip(b0,r_s)])
-    u_S = np.array([fixation_prob(1/b,f*r,Z) for Z,r,b in zip(Zs,r_s,CC*b0)])
-
-    calc_est_times = np.array(1 / (mu*b0* u_S))
-    calc_est_times[ np.isnan(calc_est_times )] =0
-    return calc_est_times, u_S
-    
